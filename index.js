b4e754418e125c9735e356ad8ce94f60351d47ee
Abdullahi <aadahiye@gmail.com>
2024-09-26 23:35:17 +0300

    Completes lab on looping

--- index.js
+++ index.js
@@ -1,1 +1,14 @@
-// Code your solutions in this file
+function writeCards(arr, name) {
+    let newArr = [];
+    for (let i = 0; i < arr.length; i++) {
+        newArr.push(`Thank you, ${arr[i]}, for the wonderful ${name} gift!`);
+    }
+    return newArr;
+}
+
+
+function countDown(num) {
+    for (let i = num; i >= 0; i--) {
+        console.log(i);
+    }
+}
